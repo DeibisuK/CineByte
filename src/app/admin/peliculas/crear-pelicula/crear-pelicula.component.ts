7f59ad279a56f99d76280e809d8fd9835c876144
DeibisuK <jhongamer530@gmail.com>
2025-06-22 12:03:26 -0500

    aumentando funciones en crear-pelicula

--- src/app/admin/peliculas/crear-pelicula/crear-pelicula.component.ts
+++ src/app/admin/peliculas/crear-pelicula/crear-pelicula.component.ts
@@ -12,9 +12,16 @@ import { Pelicula } from '../../models/pelicula.model';
 export class CrearPeliculaComponent {
 
   peliculaForm: FormGroup;
+  generos: string[] = ['Acción', 'Comedia', 'Drama', 'Terror', 'Ciencia Ficción'];
+  etiquetas: string[] = ['Clasico', 'Exclusivo', 'Estreno', '3D'];
+  dropdownGenerosOpen = false;
+  dropdownEtiquetasOpen = false;
+  selectedGeneros: string[] = [];
+  selectedEtiquetas: string[] = [];
+
   constructor() {
     this.peliculaForm = new FormGroup({
-      id:  new FormControl('', Validators.required),
+      id: new FormControl('', Validators.required),
       titulo: new FormControl('', Validators.required),
       descripcion: new FormControl('', Validators.required),
       duracion: new FormControl('', Validators.required),
@@ -26,12 +33,47 @@ export class CrearPeliculaComponent {
   }
   onSubmit() {
     if (this.peliculaForm.valid) {
-        const pelicula: Pelicula = this.peliculaForm.value as Pelicula;
+      const pelicula: Pelicula = this.peliculaForm.value as Pelicula;
       // Aquí puedes manejar el envío del formulario
       console.log('Formulario enviado:', pelicula);
     } else {
       console.log('Formulario inválido');
     }
+  }
+
+  toggleDropdownGeneros() {
+    this.dropdownGenerosOpen = !this.dropdownGenerosOpen;
+    if (this.dropdownGenerosOpen) this.dropdownEtiquetasOpen = false;
+  }
+  toggleDropdownEtiquetas() {
+    this.dropdownEtiquetasOpen = !this.dropdownEtiquetasOpen;
+    if (this.dropdownEtiquetasOpen) this.dropdownGenerosOpen = false;
+  }
+
+  onGeneroChange(genero: string, event: any) {
+    if (event.target.checked) {
+      this.selectedGeneros.push(genero);
+    } else {
+      this.selectedGeneros = this.selectedGeneros.filter(g => g !== genero);
+    }
+    this.peliculaForm.get('genero')?.setValue(this.selectedGeneros);
+  }
+  onEtiquetaChange(etiqueta: string, event: any) {
+    if (event.target.checked) {
+      this.selectedEtiquetas.push(etiqueta);
+    } else {
+      this.selectedEtiquetas = this.selectedEtiquetas.filter(e => e !== etiqueta);
+    }
+    this.peliculaForm.get('etiquetas')?.setValue(this.selectedEtiquetas);
+  }
+
+  get generosInvalid() {
+    const control = this.peliculaForm.get('genero');
+    return control?.invalid && (control?.touched || control?.dirty);
+  }
+  get etiquetasInvalid() {
+    const control = this.peliculaForm.get('etiquetas');
+    return control?.invalid && (control?.touched || control?.dirty);
   }
 
 }
