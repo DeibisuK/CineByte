c5af0dd4db6d367ed4a2fcf5da574b3c9e36a951
DeibisuK <jhongamer530@gmail.com>
2025-06-23 10:02:51 -0500

    Se modifico el codigo de crear y listar peliculas

--- src/app/admin/models/pelicula.model.ts
+++ src/app/admin/models/pelicula.model.ts
@@ -3,7 +3,8 @@ export interface Pelicula {
     titulo: string;
     descripcion: string;
     duracion_minutos : number; 
-    clasificacion: string;
     fecha_estreno: Date;
+    estado: string;
+    clasificacion: string;
     imagen: string;
 };
