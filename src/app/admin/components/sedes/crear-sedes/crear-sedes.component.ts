33ae5553860ff261566a23f0bb3b7bac36a3e9f3
kinjirobruh96 <garystevenbarreirozambrano@gmail.com>
2025-06-27 19:32:48 -0500

    Desplazamiento automatico del Mapa de acuerdo a la ciudad

--- src/app/admin/components/sedes/crear-sedes/crear-sedes.component.ts
+++ src/app/admin/components/sedes/crear-sedes/crear-sedes.component.ts
@@ -36,12 +36,31 @@ export class CrearSedesComponent implements OnInit, AfterViewInit {
   map!: L.Map;
   marker!: L.Marker;
 
+  coordCiudades: { [id: number]: [number, number] } = {
+    1: [-0.22985, -78.52495],       // Quito
+    2: [-2.170998, -79.922359],     // Guayaquil
+    3: [-2.90055, -79.00453],       // Cuenca
+    4: [-0.96769, -80.70891],       // Manta
+    5: [-3.258, -79.955],           // Machala
+    6: [-1.25434, -78.62289],       // Ambato
+    7: [-1.66472, -78.65459],       // Riobamba
+    8: [-3.99313, -79.20422],       // Loja
+    9: [0.35171, -78.12233],        // Ibarra
+   10: [0.96818, -79.65172],        // Esmeraldas
+   11: [-1.80217, -79.53447],       // Babahoyo
+   12: [-2.22494, -80.85949],       // Santa Elena
+   13: [-0.25342, -79.17195]        // Santo Domingo
+  };
+
+
   constructor(private sedeService: SedeService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.sedeService.getCiudades().subscribe(data => {
       this.ciudades = data;
+      console.log('Ciudades cargadas:', this.ciudades);
     });
+
   }
 
   ngAfterViewInit(): void {
@@ -70,19 +89,18 @@ export class CrearSedesComponent implements OnInit, AfterViewInit {
 }
 
 
-  onCiudadChange(id_ciudad: number | string) {
-    const id = Number(id_ciudad); // <-- Asegura que sea número
-    const ciudad = this.ciudades.find(c => c.id_ciudad === id);
-    console.log('Ciudad seleccionada:', ciudad);
-  
-    if (ciudad?.latitud && ciudad?.longitud) {
-      const coords: [number, number] = [ciudad.latitud, ciudad.longitud];
+  onCiudadChange(id_ciudad: number) {
+    const coords = this.coordCiudades[id_ciudad];
+    if (coords) {
       this.map.setView(coords, 14);
       this.marker.setLatLng(coords);
       this.sede.latitud = coords[0];
       this.sede.longitud = coords[1];
+    } else {
+      console.warn('Ciudad sin coordenadas definidas.');
     }
   }
+
   
 
   onSubmit() {
