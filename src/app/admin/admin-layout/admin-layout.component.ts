de5263d8d4eea0dffd09342516e6e9c315add458
kinjirobruh96 <garystevenbarreirozambrano@gmail.com>
2025-07-04 10:29:54 -0500

    Prueba 3k si guardda

--- src/app/admin/admin-layout/admin-layout.component.ts
+++ src/app/admin/admin-layout/admin-layout.component.ts
@@ -1,16 +1,42 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { LoadingComponent } from '../../core/components/loading/loading.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, LoadingComponent, CommonModule],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css',
 })
-export class AdminLayoutComponent {
-  sidebarClosed = false; // <-- Agrega esta propiedad
+export class AdminLayoutComponent implements OnInit {
+  sidebarClosed = false;
+  isLoading = true;
+  loadingFadeOut = false;
+
+  constructor(private router: Router) {}
+
   toggleMobileSidebar(): void {
     this.sidebarClosed = !this.sidebarClosed;
   }
+
+ngOnInit(): void {
+  // Mostrar la pantalla de carga durante 3 segundos
+  setTimeout(() => {
+    this.loadingFadeOut = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 600); // Tiempo para aplicar el fade-out
+  }, 1500); // 1,5 segundos de carga
+}
+
+  
 }
